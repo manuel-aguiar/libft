6e2a7ef8a8004e11079656029dde5b64ff553671
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2023-05-09 16:15:03 +0100

    Add files via upload

---
+++ srcs/legacy/ft_lstadd_back.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/15 20:27:18 by manuel            #+#    #+#             */
+/*   Updated: 2023/04/19 14:10:21 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*tail;
+
+	if (!*lst)
+		*lst = new;
+	else
+	{
+		tail = *lst;
+		while (tail->next)
+			tail = tail->next;
+		tail->next = new;
+	}
+}*/
+
