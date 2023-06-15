3a0f35ebe0616a8b99c52794b72aa946420b7b66
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2023-06-15 10:45:48 +0100

    Add files via upload

---
+++ srcs/ft_memfunc/ft_free_split.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf_parsing_utils.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/15 10:25:41 by marvin            #+#    #+#             */
+/*   Updated: 2023/06/15 10:25:41 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_memfunc.h"
+
+void	free_split(void *to_free)
+{
+	char	**split;
+	int		i;
+
+	if (!to_free)
+		return ;
+	split = (char **)to_free;
+	i = 0;
+	while (split[i])
+	{
+		free(split[i]);
+		i++;
+	}
+	free(split);
+}
